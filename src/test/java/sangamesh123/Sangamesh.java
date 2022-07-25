6ece6412903a25c836683a2c01f0f400cc5cb07f
Sangamesh0097 <sangamesh0097@gmail.com>
2022-07-25 13:14:53 +0530

    fhsjfh

--- src/test/java/sangamesh123/Sangamesh.java
+++ src/test/java/sangamesh123/Sangamesh.java
@@ -3,7 +3,8 @@ package sangamesh123;
 public class Sangamesh {
 	public static void main(String[] args) {
 		System.out.println("hiiiiii");
-		
+		System.out.println("test yantra!!!");
+		System.out.println("Youuuuu");
 
 	}
 
