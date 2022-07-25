1cee746a70495242945b4c830aa4c2f575c485ad
Sangamesh0097 <sangamesh0097@gmail.com>
2022-07-25 12:53:12 +0530

    added

---
+++ src/test/java/sangamesh123/Sangamesh.java
@@ -0,0 +1,11 @@
+package sangamesh123;
+
+public class Sangamesh {
+	public static void main(String[] args) {
+		System.out.println("hiiiiii");
+		System.out.println("test yantra!!!");
+		System.out.println("Youuuuu");
+
+	}
+
+}
