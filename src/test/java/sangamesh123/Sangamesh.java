851e76f55ce78771a6574b6bb61fe547629f17bb
Sangamesh0097 <96580425+Sangamesh0097@users.noreply.github.com>
2022-07-25 15:23:26 +0530

    Update Sangamesh.java

--- src/test/java/sangamesh123/Sangamesh.java
+++ src/test/java/sangamesh123/Sangamesh.java
@@ -2,9 +2,7 @@ package sangamesh123;
 
 public class Sangamesh {
 	public static void main(String[] args) {
-		System.out.println("hiiiiii");
-		System.out.println("test yantra!!!");
-		System.out.println("Youuuuu");
+	
 
 	}
 
